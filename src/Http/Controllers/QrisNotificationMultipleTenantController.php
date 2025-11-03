b5097a92599156e8a2789e26ba61c7f0def6609f
Bayu <bayu@elgibor-solution.com>
2025-11-03 16:46:06 +0700

    feat(qris): add transaction request field validation rules

--- src/Http/Controllers/QrisNotificationMultipleTenantController.php
+++ src/Http/Controllers/QrisNotificationMultipleTenantController.php
@@ -7,11 +7,49 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Event;
 use ESolution\BriPayments\Support\SnapSignature;
 use ESolution\BriPayments\Events\QrisPaymentNotified;
+use Illuminate\Support\Facades\Validator;
 
 class QrisNotificationMultipleTenantController extends Controller
 {
     public function handle(Request $request, SnapSignature $sig, $tenant)
     {
+
+        $validator = Validator::make($request->all(), [
+            'originalReferenceNo'         => ['required', 'string', 'max:12'],
+            'originalPartnerReferenceNo'  => ['required', 'string', 'max:6'],
+            'latestTransactionStatus'     => ['nullable', 'string', 'size:2', 'in:00,01,02,03,04,05,06,07'],
+            'transactionStatusDesc'       => ['nullable', 'string', 'max:50'],
+            'customerNumber'              => ['required', 'string', 'max:64'],
+            'accountType'                 => ['nullable', 'string', 'max:25'],
+            'destinationAccountName'      => ['required', 'string', 'max:25'],
+            'amount'                      => ['required', 'array'],
+            'amount.value'                => ['required', 'numeric', 'min:0'],
+            'amount.currency'             => ['required', 'string', 'size:3'],
+            'bankCode'                    => ['nullable', 'string', 'max:8'],
+            'additionalInfo'              => ['nullable', 'array'],
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            $firstKey = array_key_first($errors);
+            $failedRules = $validator->failed()[$firstKey] ?? [];
+
+            // Tentukan kode error
+            if (isset($failedRules['Required'])) {
+                // Invalid Mandatory Field
+                return response()->json([
+                    'responseCode'    => '4005202', // HTTP 400 + Service 52 + 02
+                    'responseMessage' => "Invalid Mandatory Field {$firstKey}",
+                ], 400);
+            }
+
+            // Invalid Format
+            return response()->json([
+                'responseCode'    => '4005201',
+                'responseMessage' => "Invalid Field Format {$firstKey}",
+            ], 400);
+        }
+
         $headers = [
             'Authorization' => $request->header('Authorization'),
             'X-SIGNATURE'   => $request->header('X-SIGNATURE'),
@@ -24,6 +62,6 @@ class QrisNotificationMultipleTenantController extends Controller
         $stringToSign = $clientId . '|' . ($headers['X-TIMESTAMP'] ?? '');
         $valid = $sig->verifyRsa($stringToSign, $headers['X-SIGNATURE'] ?? '');
         Event::dispatch(new QrisPaymentNotified($request->all(), $headers, $valid, $tenant));
-        return response()->json([ 'responseCode' => '2005200', 'responseMessage' => 'Successful' , 'additionalInfo'=> $request->additionalInfo??[]]);
+        return response()->json([ 'responseCode' => '2005200', 'responseMessage' => 'Successful' , 'additionalInfo'=> $request->additionalInfo??[]], 200);
     }
 }
